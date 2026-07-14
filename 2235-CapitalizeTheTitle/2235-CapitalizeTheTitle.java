3b767809575c39beee3d1b5c389c043b08352c50
dhevikakumar <148612848+dhevikakumar@users.noreply.github.com>
2026-07-14 14:15:40 +0530

    Create 2235-CapitalizeTheTitle

---
+++ 2235-CapitalizeTheTitle/2235-CapitalizeTheTitle.java
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 2:15:42 PM
+class Solution {
+    public String capitalizeTitle(String title) {
+        String[] words = title.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].toLowerCase();
+
+            if (words[i].length() > 2) {
+                words[i] = Character.toUpperCase(words[i].charAt(0))
+                        + words[i].substring(1);
+            }
+        }
+
+        return String.join(" ", words);
+    }
+}
